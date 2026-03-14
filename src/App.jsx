323da7a69a932c35a929c633e1a3c1849c916dd3
laizu01993 <laizuaktar6@gmail.com>
2026-03-14 12:47:54 +0600

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+          <h1 className='text-red-800
+        '>Get started</h1>
+          
+
+      
+     
+    </>
+  )
+}
+
+export default App
